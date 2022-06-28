7f5c7f8c840e3630090bad3448b229d362fb928d
Jhonattan Nascimento <jhonattan.nascimento.barbosa@gmail.com>
2022-06-27 22:28:03 -0300

    teste 2

--- atividade01/main.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./config/express")();
-const db = require("./config/database");
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Servidor rodando');
-});
- 
-db("mongodb://localhost/sistemarede")
